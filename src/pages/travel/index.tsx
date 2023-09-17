90ae1e75d6f8b9dca4b476cb38ee8ed3911f2062
Jerson <solorsanolopez@gmail.com>
2023-09-17 16:24:27 -0700

    🐞 [Fix] styledComponents error

--- src/pages/travel/index.tsx
+++ src/pages/travel/index.tsx
@@ -5,7 +5,25 @@ import { MapStyles } from "@/utils/mapStyles";
 import Loader from "@/components/Loader";
 import { MDXMessage } from "@/components/MDXMessage";
 import useController from "./useController";
-import { DialogBox, DividerVerticalLine } from "./styledComponents";
+import styled from "styled-components";
+
+export const DialogBox = styled.div`
+  width: 45vw;
+  height: auto;
+  background-color: #a4a4a4ba;
+  border-radius: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+`;
+
+export const DividerVerticalLine = styled.div`
+  width: 245px;
+  height: 3px;
+  background-color: #00e1ff;
+`;
 
 const Travel = () => {
   const {
